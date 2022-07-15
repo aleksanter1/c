c2c7de05c9cf840b0dfa7906d24ab1e45942e2ab
alekanter1 <aleksanter1@proton.me>
2022-07-15 10:53:18 +0300

    Added parse_options function.

---
+++ parse_options.c
@@ -0,0 +1,26 @@
+/* Function parses through passed string array for options and returns integer, where every bit represents
+one letter in alphabetical order. Function stops parsing if the string does not start with a '-' character.
+Add -lm option when compiling to link libm. */
+
+#include <math.h>
+
+#define ALPHABET "abcdefghijklmnopqrstuvwxyz"
+
+int parse_options(char **argv) {
+	int options = 0;
+	char index;
+
+	for (argv; *argv; argv += 1) {
+		if (**argv != 45)
+			break;
+		for (*argv; **argv; *argv += 1) {
+			index = 0;
+			for (index; ALPHABET[index] && **argv; index += 1)
+				if (**argv == ALPHABET[index]) {
+					options |= (int)pow(2, index);
+					break;
+				}
+		}
+	}
+	return options;
+}
