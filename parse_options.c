c42704d05d9a0c8d364288149b5b295a192b18ea
aleksanter1 <aleksanter1@proton.me>
2022-07-18 15:26:43 +0300

    Function parse_options now stops and moves to the next string if it encounters other than alphabet during current string iteration. Edited comment.

--- parse_options.c
+++ parse_options.c
@@ -1,8 +1,10 @@
 /* Function parses through passed string array for letters and returns integer, where every bit represents
-one letter in alphabetical order. Function stops parsing if the string does not start with a '-' character.
-Add -lm option when compiling to link libm. */
+one letter in alphabetical order. The function stops parsing and returns if the string does not start with
+a '-' character. The function stops and moves to the next string if it encounters other than alphabet during
+string iteration. Add -lm option when compiling to link libm. */
 
 #include <math.h>
+#include <ctype.h>
 
 #define ALPHABET "abcdefghijklmnopqrstuvwxyz"
 
@@ -13,7 +15,8 @@ int parse_options(char **argv) {
 	for (argv; *argv; argv += 1) {
 		if (**argv != 45)
 			break;
-		for (*argv; **argv; *argv += 1) {
+		*argv += 1;
+		for (*argv; isalpha(**argv); *argv += 1) {
 			index = 0;
 			for (index; ALPHABET[index] && **argv; index += 1)
 				if (**argv == ALPHABET[index]) {
