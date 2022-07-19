461242f6c63c2e5ea481fffe11fbb999ecfc5738
aleksanter1 <aleksanter1@proton.me>
2022-07-19 10:50:09 +0300

    Function parse_options now changes the address where argv is pointing.

--- parse_options.c
+++ parse_options.c
@@ -1,25 +1,26 @@
 /* Function parses through passed string array for letters and returns integer, where every bit represents
 one letter in alphabetical order. The function stops parsing and returns if the string does not start with
 a '-' character. The function stops and moves to the next string if it encounters other than alphabet during
-string iteration. Add -lm option when compiling to link libm. */
+string parsing. Add -lm option when compiling to link libm. */
 
 #include <math.h>
 #include <ctype.h>
 
 #define ALPHABET "abcdefghijklmnopqrstuvwxyz"
 
-int parse_options(char **argv) {
+int parse_options(char ***argv) {
 	int options = 0;
 	char index;
 
-	for (argv; *argv; argv += 1) {
-		if (**argv != 45)
+	*argv += 1;
+	for (*argv; **argv; *argv += 1) {
+		if (***argv != 45)
 			break;
-		*argv += 1;
-		for (*argv; isalpha(**argv); *argv += 1) {
+		**argv += 1;
+		for (**argv; isalpha(***argv); **argv += 1) {
 			index = 0;
-			for (index; ALPHABET[index] && **argv; index += 1)
-				if (**argv == ALPHABET[index]) {
+			for (index; ALPHABET[index] && ***argv; index += 1)
+				if (***argv == ALPHABET[index]) {
 					options |= (int)pow(2, index);
 					break;
 				}
